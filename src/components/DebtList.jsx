23d5996dc0438192d41581cdb804db4b63a31069
Logan <saibor.p.ly@hotmail.com>
2026-05-19 19:49:05 -0600

    Correccion de lista

--- src/components/DebtList.jsx
+++ src/components/DebtList.jsx
@@ -1,14 +1,16 @@
 import { DebtCard } from "./DebtCard"
 
-export function DebtList ({allDebts}) {
+export function DebtList ({ allDebts }) {
   return (
     <>
         <h2>Listado</h2>
-        <ul>
-            {allDebts.map((debt,index) => (
-                <li key={index}>{DebtCard({debt})}</li>
+        {/* Usamos un div en lugar de un ul/li para que sea más fácil darle estilos de tarjeta después */}
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+            {allDebts.map((debt, index) => (
+                // Mandamos a llamar al componente como etiqueta JSX
+                <DebtCard key={index} debt={debt} />
             ))}
-        </ul>
+        </div>
     </>
   )
 }
